2811ceabbacbb09da646c86de3869b6d0d5eba22
MobileTesting-15 <deliaxdchen@outlook.com>
2019-01-06 12:51:05 +0800

    added code for deviceinformation

---
+++ miniprogram/pages/deviceInfo/deviceInfo.js
@@ -0,0 +1,72 @@
+// pages/deviceInfo/deviceInfo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    picUrl: 'cloud://testcloud-27877a.7465-testcloud-27877a/deviceImage/default.jpeg',
+    
+    name_title: 'Device Name:',
+    name: 'GZ_Macbook Pro_F',
+
+    os_title: 'System:',
+    os: 'Mac OS 10.13.3'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
